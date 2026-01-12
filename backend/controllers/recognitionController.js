bd27923692e74431287a534e168139988918bb50
Ananda Agung Ismail <anandaagungismail@gmail.com>
2026-01-13 01:29:14 +0700

    Add module imports for recognitionController

---
+++ backend/controllers/recognitionController.js
@@ -0,0 +1,5 @@
+const RecognitionLog = require('../models/RecognitionLog');
+const RegisteredFace = require('../models/RegisteredFace');
+const { spawn } = require('child_process');
+const path = require('path');
+const fs = require('fs');
