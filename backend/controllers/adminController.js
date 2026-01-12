16ec2937946cb2224b1601ea8cbd2db6a49e45b7
Ananda Agung Ismail <anandaagungismail@gmail.com>
2026-01-13 01:17:19 +0700

    Add getUsers controller function

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -3,3 +3,12 @@ const User = require('../models/User');
 const ApiKey = require('../models/ApiKey');
 const RegisteredFace = require('../models/RegisteredFace');
 const RecognitionLog = require('../models/RecognitionLog');
+
+exports.getUsers = async (req, res) => {
+  try {
+    const users = await User.findAll({ attributes: { exclude: ['password'] }, order: [['createdAt', 'DESC']] });
+    res.status(200).json({ users });
+  } catch (err) {
+    res.status(500).json({ message: 'Server error: ' + err.message });
+  }
+};
