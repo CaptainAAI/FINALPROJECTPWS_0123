e84f74b292464b7f51c540fe3299425beef99fbb
Ananda Agung Ismail <anandaagungismail@gmail.com>
2026-01-13 01:17:07 +0700

    Add module imports and dependencies

---
+++ backend/controllers/adminController.js
@@ -0,0 +1,5 @@
+const { Op } = require('sequelize');
+const User = require('../models/User');
+const ApiKey = require('../models/ApiKey');
+const RegisteredFace = require('../models/RegisteredFace');
+const RecognitionLog = require('../models/RecognitionLog');
