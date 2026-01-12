dedf46708e1175376b9f06fd1b0e49cafc11b30d
Ananda Agung Ismail <anandaagungismail@gmail.com>
2026-01-13 01:25:16 +0700

    Add getAll controller function

--- backend/controllers/faceController.js
+++ backend/controllers/faceController.js
@@ -1,2 +1,19 @@
 const RegisteredFace = require('../models/RegisteredFace');
 const fs = require('fs');
+
+// Get all registered faces
+exports.getAll = async (req, res) => {
+  try {
+    const userId = req.userId;
+
+    const faces = await RegisteredFace.findAll({
+      where: { userId, isActive: true },
+      attributes: { exclude: ['embedding', 'imagePath'] },
+      order: [['createdAt', 'DESC']]
+    });
+
+    return res.status(200).json({ faces });
+  } catch (error) {
+    return res.status(500).json({ message: 'Server error: ' + error.message });
+  }
+};
